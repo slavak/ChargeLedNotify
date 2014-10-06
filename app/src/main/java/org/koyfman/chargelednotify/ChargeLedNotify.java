b022cf5771365e06b92807e1f3a544a1f332dc0d
slavak <none@none>
2014-10-06 21:42:57 +0300

    Removed the logLightStatus method- it's useless for tracking changes made by calling setLight_native directly. Cleaned up some of the code structure and log messages; added Javadoc comments.

--- app/src/main/java/org/koyfman/chargelednotify/ChargeLedNotify.java
+++ app/src/main/java/org/koyfman/chargelednotify/ChargeLedNotify.java
@@ -1,50 +1,64 @@
 package org.koyfman.chargelednotify;
 
 import android.graphics.Color;
+
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 
 public class ChargeLedNotify implements IXposedHookZygoteInit {
+    static final int LIGHT_ID_BACKLIGHT = 0;
+    static final int LIGHT_ID_KEYBOARD = 1;
+    static final int LIGHT_ID_BUTTONS = 2;
+    static final int LIGHT_ID_BATTERY = 3;
+    static final int LIGHT_ID_NOTIFICATIONS = 4;
+    static final int LIGHT_ID_ATTENTION = 5;
+    static final int LIGHT_ID_BLUETOOTH = 6;
+    static final int LIGHT_ID_WIFI = 7;
+    static final int LIGHT_ID_COUNT = 8;
+
+    static final String[] LIGHT_ID2NAME = {
+        "BACKLIGHT",
+        "KEYBOARD",
+        "BUTTONS",
+        "BATTERY",
+        "NOTIFICATIONS",
+        "ATTENTION",
+        "BLUETOOTH",
+        "WIFI",
+        "COUNT"
+    };
+
     private static final boolean DEBUG = BuildConfig.DEBUG;
     private static final String PKGNAME = ChargeLedNotify.class.getPackage().getName();
     private static final String CLASS_LIGHT_SERVICE_LIGHT = "com.android.server.LightsService$Light";
-
-    private static final int LIGHT_ID_BATTERY = 3;
-    private static final int LIGHT_ID_NOTIFICATIONS = 4;
 
     private static Object mLightsService;
     private static int mNativePointer;
     private static boolean mLedEnabled = false;
 
-    // For debug logging only
+    /**
+     * Log message to Xposed log. Log will include identifying information (package name).
+     * @param m Message to log
+     */
     private static void log(String m)    { XposedBridge.log(PKGNAME + ": " + m); }
 
+    /**
+     * Turn the charging LED on or off.
+     * @param enable    Charging LED will be turned on if true, or off otherwise.
+     */
     private static void setChargeLED(boolean enable) {
         if (mLightsService == null) return;
         int color = enable ? Color.RED : 0;
 
         try {
-            if (DEBUG) log("setLight_native(color=" + color + ")");
+            if (DEBUG) log("setLight_native(BATTERY, color=" + color + ")");
             XposedHelpers.callMethod(mLightsService, "setLight_native",
                                      mNativePointer, LIGHT_ID_BATTERY, color, 0, 0, 0, 0);
         } catch (Throwable t) {
             XposedBridge.log(t);
         }
-    }
-
-    private static void logLightStatus(int lightId) {
-        Object light = XposedHelpers.callMethod(mLightsService, "getLight", lightId);
-        int color = XposedHelpers.getIntField(light, "mColor");
-        int mode = XposedHelpers.getIntField(light, "mMode");
-        int onMS = XposedHelpers.getIntField(light, "mOnMS");
-        int offMS = XposedHelpers.getIntField(light, "mOffMS");
-        boolean flashing = XposedHelpers.getBooleanField(light, "mFlashing");
-        log("Status for light=" + lightId + ": " +
-            "color=" + color + "; mode=" + mode +
-            "; onMS=" + onMS + "; offMS=" + offMS +
-            "; flashing=" + flashing);
     }
 
     private static void enableChargeLED()  { setChargeLED(true); }
@@ -67,7 +81,7 @@ public class ChargeLedNotify implements IXposedHookZygoteInit {
                         int id = XposedHelpers.getIntField(param.thisObject, "mId");
                         int color = (Integer)param.args[0];
 
-                        if (DEBUG) log("lightId=" + id + "; color=" + color +
+                        if (DEBUG) log("lightId=" + id + "(" + LIGHT_ID2NAME[id] + "); color=" + color +
                                        "; mode=" + param.args[1] + "; " + "onMS=" + param.args[2] +
                                        "; offMS=" + param.args[3] + "; bMode=" + param.args[4]);
 
@@ -91,9 +105,8 @@ public class ChargeLedNotify implements IXposedHookZygoteInit {
                                 // Pass-through unrelated light events unhindered
                                 break;
                         }
-
-                        if (DEBUG) logLightStatus(LIGHT_ID_BATTERY);
                     }
+
                 });
     }
 }
